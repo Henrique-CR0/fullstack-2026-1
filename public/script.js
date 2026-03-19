347631fa943b6c77b04bf127b0c9f281e046f187
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2026-03-19 22:00:48 +0000

    criando front do projeto fullstack

---
+++ public/script.js
@@ -0,0 +1,42 @@
+ function carregar(){
+        fetch("https://urban-rotary-phone-p4v44r9jjrw2r7pp-3000.app.github.dev/alunos")
+            .then((res) => {
+                return res.json()
+            })
+            .then((dados) => {
+                console.log("OS DADOS AQUIIIIIIII",dados)
+                for(let dado of dados){
+                    document.querySelector("ul").innerHTML += ` <li class="list-group-item">${dado.nome}</li>`
+                }
+                    
+            })
+    }
+
+    carregar()
+
+    // função salvar aluno
+    function salvar() {
+        aluno = {
+            nome: document.querySelector("#nome").value,
+            idade: document.querySelector("#idade").value,
+            sexo: document.querySelector("#sexo").value
+        }
+        console.log("aluno para salvar", aluno)
+
+        fetch("https://urban-rotary-phone-p4v44r9jjrw2r7pp-3000.app.github.dev/alunos", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(aluno)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Sucesso:", data)
+                document.querySelector("ul").innerHTML = ""
+                carregar()
+            })
+            .catch(error => {
+                console.error("Erro:", error)
+            })
+    }
