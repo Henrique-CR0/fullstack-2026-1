ceb13d0921c8af7398d91dc89b3026fef7a30b3f
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2026-03-19 22:49:23 +0000

    ajustando HTML e SCRIPT do projeto

--- public/script.js
+++ public/script.js
@@ -1,42 +1,45 @@
- function carregar(){
-        fetch("https://urban-rotary-phone-p4v44r9jjrw2r7pp-3000.app.github.dev/alunos")
-            .then((res) => {
-                return res.json()
-            })
-            .then((dados) => {
-                console.log("OS DADOS AQUIIIIIIII",dados)
-                for(let dado of dados){
-                    document.querySelector("ul").innerHTML += ` <li class="list-group-item">${dado.nome}</li>`
-                }
-                    
-            })
-    }
+// colem aqui a URL gerada no seu projeto do codespace
+const url = "https://urban-rotary-phone-p4v44r9jjrw2r7pp-3000.app.github.dev/alunos"
 
-    carregar()
-
-    // função salvar aluno
-    function salvar() {
-        aluno = {
-            nome: document.querySelector("#nome").value,
-            idade: document.querySelector("#idade").value,
-            sexo: document.querySelector("#sexo").value
-        }
-        console.log("aluno para salvar", aluno)
-
-        fetch("https://urban-rotary-phone-p4v44r9jjrw2r7pp-3000.app.github.dev/alunos", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(aluno)
+function carregar() {
+    fetch(url)
+        .then((res) => {
+            return res.json()
         })
-            .then(response => response.json())
-            .then(data => {
-                console.log("Sucesso:", data)
-                document.querySelector("ul").innerHTML = ""
-                carregar()
-            })
-            .catch(error => {
-                console.error("Erro:", error)
-            })
+        .then((dados) => {
+            console.log("OS DADOS AQUIIIIIIII", dados)
+            for (let dado of dados) {
+                document.querySelector("ul").innerHTML += ` <li class="list-group-item">${dado.nome}</li>`
+            }
+
+        })
+}
+
+carregar()
+
+// função salvar aluno
+function salvar() {
+    aluno = {
+        nome: document.querySelector("#nome").value,
+        idade: document.querySelector("#idade").value,
+        sexo: document.querySelector("#sexo").value
     }
+    console.log("aluno para salvar", aluno)
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(aluno)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Sucesso:", data)
+            document.querySelector("ul").innerHTML = ""
+            carregar()
+        })
+        .catch(error => {
+            console.error("Erro:", error)
+        })
+}
